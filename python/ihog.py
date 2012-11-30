365570204386426f0401d26bd72a29b188d41e33
Carl Vondrick <vondrick@mit.edu>
2012-11-30 15:50:02 -0500

    foo

--- python/ihog.py
+++ python/ihog.py
@@ -1,2 +1,12 @@
-def invertHOG(feat):
+"""
+This file implements the paired dictionary inference algorithm for HOG inversion
+with Python. For learning the dictionary, see the MATLAB port.
+"""
+
+import spams
+
+def invertHOG(feat, pd = None):
+    alpha = spams.lasso(windows, dhog, lambda1 = lambda1, mode = 2)
+
+if __name__ == "__main__":
     pass
