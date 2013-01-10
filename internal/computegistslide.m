251a25191764c50ced5033fdb5fd6effd92d25a1
Carl Vondrick <vondrick@mit.edu>
2013-01-09 23:10:04 -0500

    gist on csail cluster

--- internal/computegistslide.m
+++ internal/computegistslide.m
@@ -1,29 +1,37 @@
 function computegistslide(source, out),
 
+matlabpool open
+
 images = dir(source);
 images = images(randperm(length(images)));
 
-for i=1:length(images),
+%matlabpool open
+
+parfor i=1:length(images),
   I = images(i);
 
   if I.isdir,
     continue;
   end
 
-  if exist(sprintf('%s/%s.lock', source, I.name), 'dir'),
+  if exist(sprintf('%s/%s.mat', out, I.name), 'file'),
+    fprintf('igist: skipping %s because output already exists\n', I.name);
+    continue;
+  end
+
+  if exist(sprintf('%s/%s.lock', source, I.name)),
     fprintf('igist: skipping %s because lock file exists\n', I.name);
     continue;
   end
 
-  if exist(sprintf('%s/%s.mat', out, I.name), 'file'),
-    fprintf('igist: skipping %s because output already exists', I.name);
-    continue;
-  end
+  mkdir(sprintf('%s/%s.lock', source, I.name));
 
   fprintf('igist: processing %s\n', I.name);
 
   im = double(imread(sprintf('%s/%s', source, I.name))) / 255.;
   im = mean(im,3);
+
+  data = cell(0);
 
   gdim = 128;
   skipby = 32;
@@ -37,5 +45,14 @@ for i=1:length(images),
     end
   end
 
-  save(sprintf('%s/%s.mat', out, I.name), 'data');
+  fprintf('igist: writing %s/%s.mat\n', out, I.name);
+  opaquesave(sprintf('%s/%s.mat', out, I.name), data);
+  
+  try,
+    rmdir(sprintf('%s/%s.lock', source, I.name));
+  catch 
+  end
 end
+
+function opaquesave(filename, data),
+  save(filename, 'data');
