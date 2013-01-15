b33619a1aa273dff337f11b5efb278899d79a21b
Carl Vondrick <vondrick@mit.edu>
2013-01-15 07:42:57 -0500

    added fast mode to master

--- internal/learnpairdict.m
+++ internal/learnpairdict.m
@@ -11,12 +11,13 @@
 %   lambda    Sparsity regularization parameter on alpha
 %   iters     Number of iterations 
 %   sbin      The HOG bin size
+%   fast      If true, 'learn' a dictionary in real time (default false)
 % 
 % Returns a struct with fields:
 %   dgray     A dictionary of gray elements
 %   dhog      A dictionary of HOG elements
 
-function pd = learnpairdict(stream, n, k, ny, nx, lambda, iters, sbin),
+function pd = learnpairdict(stream, n, k, ny, nx, lambda, iters, sbin, fast),
 
 if ~exist('n', 'var'),
    n = 100000;
@@ -34,10 +35,13 @@ if ~exist('lambda', 'var'),
   lambda = 0.02; % 0.02 is best so far
 end
 if ~exist('iters', 'var'),
-  iters = 2000;
+  iters = 1000;
 end
 if ~exist('sbin', 'var'),
   sbin = 8;
+end
+if ~exist('fast', 'var'),
+  fast = false;
 end
 
 graysize = (ny+2)*(nx+2)*sbin^2;
@@ -50,7 +54,11 @@ data = getdata(stream, n, [ny nx], sbin);
 data(1:graysize, :) = whiten(data(1:graysize, :));
 data(graysize+1:end, :) = whiten(data(graysize+1:end, :));
 
-dict = lasso(data, k, iters, lambda);
+if fast,
+  dict = pickrandom(data, k); 
+else,
+  dict = lasso(data, k, iters, lambda);
+end
 
 pd.dgray = dict(1:graysize, :);
 pd.dhog = dict(graysize+1:end, :);
@@ -88,6 +96,21 @@ for i=1:(iters/param.iter),
   model.iter = i*param.iter;
   param.D = dict;
 end
+
+
+
+% pickrandom(data, k)
+%
+% Picks a random 'k' elements from 'data' for fast training. This option is mostly
+% useful for debugging purposes. The learned dictionary is usually always better than
+% this mode. But, this method still produces surprisingly good reconstructions.
+function dict = pickrandom(data, k),
+
+fprintf('ihog: sampling %i random elements for dictionary instead of learning\n', k)
+order = randperm(size(data, 2));
+order = order(1:k);
+dict = data(:, order);
+
 
 
 
