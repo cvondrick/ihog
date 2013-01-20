e5ad66d916cd89baad9efa3ba6cf813c584ffb55
Carl Vondrick <vondrick@mit.edu>
2013-01-20 13:48:51 -0500

    alyosha clusterized

--- alyosha.m
+++ alyosha.m
@@ -1,17 +1,52 @@
-function im = alyosha(im, pd),
+function im = alyosha(filepath, out, pd),
 
-feat = features(im, 8);
-ihog = invertHOG(feat, pd);
+rng('shuffle');
 
-im = imresize(im, [size(ihog, 1) size(ihog, 2)]);
-im(im > 1) = 1;
-im(im < 0) = 0;
+images = dir(filepath);
+images = images(randperm(length(images)));
 
-origalt = rgb2hsv(im);
+for i=1:length(images),
+  if images(i).isdir,
+    continue;
+  end
 
-alt = rgb2hsv(ihog);
-alt(:, :, 3) = origalt(:, :, 3);
+  fprintf('%s: ', images(i).name);
 
-im = hsv2rgb(alt);
+  if exist([filepath '/' images(i).name '.lock'], 'dir'),
+    fprintf('locked\n');
+    continue;
+  end
+  if exist([out '/' images(i).name], 'file'),
+    fprintf('completed\n');
+    continue;
+  end
+  mkdir([filepath '/' images(i).name '.lock']);
+  fprintf('working...');
 
-imagesc(im); axis image;
+  im = imread([filepath '/' images(i).name]);
+  im = im2double(im);
+
+  feat = features(im, 8);
+  ihog = invertHOG(feat, pd);
+
+  im = imresize(im, [size(ihog, 1) size(ihog, 2)]);
+  im(im > 1) = 1;
+  im(im < 0) = 0;
+
+  origalt = rgb2hsv(im);
+
+  alt = rgb2hsv(ihog);
+  alt(:, :, 3) = origalt(:, :, 3);
+
+  im = hsv2rgb(alt);
+
+  imagesc(im); axis image; drawnow;
+
+  imwrite(im, [out '/' images(i).name]);
+  fprintf('processed!\n');
+
+  try,
+    rmdir([filepath '/' images(i).name '.lock']);
+  catch
+  end
+end
