eda092d6fe7d1286b9fa39cca2b97cb10992217e
Carl Vondrick <vondrick@mit.edu>
2013-09-04 16:59:50 -0400

    pd2txt

---
+++ internal/pd2txt.m
@@ -0,0 +1,18 @@
+function pd2txt(out, pd),
+
+f = fopen(out, 'w');
+fprintf(f, '%f\n', pd.lambda);
+fprintf(f, '%i %i\n', pd.ny, pd.nx);
+fprintf(f, '%i\n', pd.sbin);
+fprintf(f, '%i\n', pd.k);
+fprintf(f, '\n');
+for i=1:pd.k,
+  fprintf(f, '%f ', pd.dhog(i, :));
+  fprintf(f, '\n');
+end
+fprintf(f, '\n');
+for i=1:pd.k,
+  fprintf(f, '%f ', pd.dgray(i, :));
+  fprintf(f, '\n');
+end
+fclose(f);
