0e4619d5a94a7fa3fc17572da975b5f9665f6d75
Carl Vondrick <vondrick@mit.edu>
2013-11-19 16:47:41 -0500

    visualizing convolutional neural nets!

--- visualizeCNN.m
+++ visualizeCNN.m
@@ -1,13 +1,44 @@
-function visualizeCNN(data, pd),
+function inverse = visualizeCNN(data, pd, nx, ny),
 
-for i=1:size(data.features,1),
-  subplot(121);
-  imagesc(invertCNN(data.features(i, :), pd));
-  axis image;
+skip = 4;
 
-  subplot(122);
-  imagesc(reshape(data.images(i, :), [256 256 3]));
-  axis image;
+inverse = zeros(64+(ny-1)*skip, 64+(nx-1)*skip, 3);
+original = zeros(64+(ny-1)*skip, 64+(nx-1)*skip, 3);
+weights = zeros(64+(ny-1)*skip, 64+(nx-1)*skip);
 
-  pause(.5);
+c = 1;
+for i=1:ny,
+  for j=1:nx,
+    icnn = invertCNN(data.features(c, :), pd);
+    orig = reshape(data.images(c, :), [256 256 3]);
+    orig = im2double(imresize(orig, [64 64]));
+    iii = (i-1)*skip+1:(i-1)*skip+64;
+    jjj = (j-1)*skip+1:(j-1)*skip+64;
+    inverse(iii, jjj, :) = inverse(iii, jjj, :) + icnn;
+    original(iii, jjj, :) = original(iii, jjj, :) + orig;
+    weights(iii, jjj) = weights(iii, jjj) + 1;
+    c = c + 1;
+
+%    subplot(131);
+%    imagesc(icnn);
+%    axis image;
+%    subplot(132);
+%    imagesc(orig);
+%    axis image;
+%    title(sprintf('%i', c-1));
+%    subplot(133);
+%    imagesc(original ./ repmat(weights, [1 1 3]));
+%    pause;
+ end
 end
+
+weights = repmat(weights, [1 1 3]);
+inverse = inverse ./ weights;
+original = original ./ weights;
+
+subplot(121);
+imagesc(inverse);
+axis image;
+subplot(122);
+imagesc(original);
+axis image;
