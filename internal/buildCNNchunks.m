079dbb636a502ecb7e36e174da06ffa7ee943594
Carl Vondrick <vondrick@mit.edu>
2014-02-02 22:41:59 -0500

    out-of-core extensions

---
+++ internal/buildCNNchunks.m
@@ -0,0 +1,82 @@
+function buildCNNchunks(sourcedir, outdir, chunksize),
+
+if ~exist('chunksize', 'var'),
+  chunksize = 100000;
+end
+if ~exist('layer', 'var'),
+  layer = 'pool5_cudanet_out';
+end
+if ~exist('featdim', 'var'),
+  featdim = [6 6 256];
+end
+if ~exist('imagedim', 'var'),
+  imagedim = 64;
+end
+if ~exist('imagedimorig', 'var'),
+  imagedimorig = 256;
+end
+
+imagedimorig = 256;
+data = zeros(prod(featdim)+imagedim^2*3, chunksize, 'single');
+c = 1;
+chunkid = 1;
+n = 0;
+
+master.imdim = [imagedim imagedim 3];
+master.featdim = featdim;
+
+files = dir(sourcedir);
+files = files(randperm(length(files)));
+for i=1:length(files),
+  if files(i).isdir,
+    continue;
+  end
+
+  fprintf('icnn: load: %s (chunk has %i of %i)\n', files(i).name, c, chunksize);
+
+  payload = load([sourcedir '/' files(i).name]);
+  payloadfeatures = getfield(payload, layer);
+
+  num = size(payloadfeatures,1);
+  n = n + num;
+
+  for j=1:num,
+    im = squeeze(payload.images(j, :, :, :));
+    im = imresize(im, [imagedim imagedim]);
+    im = single(im) / 255.;
+
+    feat = payloadfeatures(j, :, :, :);
+
+    % normalize
+    im(:) = im(:) - mean(im(:));
+    im(:) = im(:) / (sqrt(sum(im(:).^2) + eps));
+
+    feat(:) = feat(:) - mean(feat(:));
+    feat(:) = feat(:) / (sqrt(sum(feat(:).^2) + eps));
+
+    data(1:imagedim^2*3, c) = im(:);
+    data(imagedim^2*3+1:end, c) = feat(:);
+
+    c = c + 1;
+
+    if c > chunksize,
+      outpath = sprintf('%s/chunk-%i.mat', outdir, chunkid);
+      fprintf('icnn: flush chunk %i to %s\n', chunkid, outpath);
+      master.files{chunkid} = sprintf('chunk-%i.mat', chunkid);
+      save(outpath, '-v7.3', 'data');
+      c = 1;
+      chunkid = chunkid + 1;
+
+      master.n = n;
+      save(sprintf('%s/master.mat', outdir));
+    end
+  end
+end
+
+data = data(:, 1:c-1);
+master.files{chunkid} = sprintf('chunk-%i.mat', chunkid);
+fprintf('icnn: flush chunk %i\n', chunkid);
+save(sprintf('%s/chunk-%i.mat', outdir, chunkid), 'data');
+
+master.n = n;
+save(sprintf('%s/master.mat', outdir));
--- internal/learnCNNdict.m
+++ internal/learnCNNdict.m
@@ -1,5 +1,4 @@
-% learnCNNdict(stream, n, k, size)
-function pd = learnCNNdict(datafile, k, lambda, iters, fast),
+function pd = learnCNNdict(chunkmasterfile, k, lambda, iters, fast),
 
 if ~exist('k', 'var'),
   k = 1024;
@@ -8,7 +7,7 @@ if ~exist('lambda', 'var'),
   lambda = 0.02; % 0.02 is best so far
 end
 if ~exist('iters', 'var'),
-  iters = 1000;
+  iters = 10;
 end
 if ~exist('fast', 'var'),
   fast = false;
@@ -16,46 +15,13 @@ end
 
 t = tic;
 
-fprintf('icnn: loading data...\n');
-load(datafile); % loads: data, imdim, featdim
+fprintf('icnn: locating chunkfiles...\n');
+master = load(chunkmasterfile);
+master = master.master;
 
-n = size(data,2);
-rgbsize = prod(imdim);
-
-fprintf('icnn: imdim=%i, featdim=%i, n=%i, k=%i\n', rgbsize, size(data,1)-rgbsize, n, k);
-
-fprintf('icnn: normalize\n');
-for i=1:size(data,2),
-  data(1:rgbsize, i) = data(1:rgbsize, i) - mean(data(1:rgbsize, i));
-  data(1:rgbsize, i) = data(1:rgbsize, i) / (sqrt(sum(data(1:rgbsize, i).^2) + eps));
-  data(rgbsize+1:end, i) = data(rgbsize+1:end, i) - mean(data(rgbsize+1:end, i));
-  data(rgbsize+1:end, i) = data(rgbsize+1:end, i) / (sqrt(sum(data(rgbsize+1:end, i).^2) + eps));
+for i=1:length(master.files),
+  fprintf('icnn: valid chunk: %s\n', master.files{i})
 end
-
-if fast,
-  dict = pickrandom(data, k); 
-else,
-  dict = lasso(data, k, iters, lambda);
-end
-
-pd.drgb = dict(1:rgbsize, :);
-pd.dcnn = dict(rgbsize+1:end, :);
-pd.n = n;
-pd.k = k;
-pd.imdim = imdim;
-pd.featdim = featdim;
-pd.iters = iters;
-pd.lambda = lambda;
-pd.feat = 'CNN';
-
-fprintf('icnn: paired dictionaries learned in %0.3fs\n', toc(t));
-
-
-
-% lasso(data)
-%
-% Learns the pair of dictionaries for the data terms.
-function dict = lasso(data, k, iters, lambda),
 
 param.K = k;
 param.lambda = lambda;
@@ -67,25 +33,26 @@ param.verbose = 1;
 param.batchsize = 400;
 param.posAlpha = true;
 
-fprintf('icnn: lasso\n');
 model = struct();
-for i=1:(iters/param.iter),
-  fprintf('icnn: lasso: master iteration #%i\n', i);
-  [dict, model] = mexTrainDL(data, param, model);
+for i=1:iters,
+  chunkid = floor(length(master.files)*rand()+1);
+  data = load(sprintf('%s/%s', fileparts(chunkmasterfile), master.files{chunkid}));
+
+  fprintf('icnn: iteration #%i: chunk=%s, n=%i\n', i, master.files{chunkid}, size(data.data,2));
+
+  [dict, model] = mexTrainDL(data.data, param, model);
   model.iter = i*param.iter;
   param.D = dict;
 end
 
+pd.drgb = dict(1:prod(master.imdim), :);
+pd.dcnn = dict(prod(master.imdim)+1:end, :);
+pd.n = master.n;
+pd.k = k;
+pd.imdim = master.imdim;
+pd.featdim = master.featdim;
+pd.chunkmaster = master;
+pd.lambda = lambda;
+pd.feat = 'CNN';
 
-
-% pickrandom(data, k)
-%
-% Picks a random 'k' elements from 'data' for fast training. This option is mostly
-% useful for debugging purposes. The learned dictionary is usually always better than
-% this mode. But, this method still produces surprisingly good reconstructions.
-function dict = pickrandom(data, k),
-
-fprintf('icnn: sampling %i random elements for dictionary instead of learning\n', k)
-order = randperm(size(data, 2));
-order = order(1:k);
-dict = data(:, order);
+fprintf('icnn: paired dictionaries learned in %0.3fs\n', toc(t));
